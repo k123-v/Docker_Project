289e122a49fcb8473caef6f63a492655af82e518
kowshikvarmag@gmail.com <kowshikvarmag@gmail.com>
2021-06-27 17:28:28 +0530

    new project for docker

---
+++ infra.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+resource "aws_instance" "customdevice" {
+  ami           = data.aws_ami.customami.id
+  instance_type = "t2.micro"
+  key_name          = "feb24key"
+  availability_zone = "us-east-1a"
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = "feb24key"
+    
+  }
+  private_ip ="172.31.90.136" 
+  tags = {
+    Name = "custompackerec2"
+  }
+  
+}
+
+data "aws_ami" "customami" {
+  most_recent      = true
+  owners           = ["self"]
+
+  filter {
+        name = "tag:Name"
+        values = ["mypackeraminew"]
+    }
+
+  
+ 
+}
+
